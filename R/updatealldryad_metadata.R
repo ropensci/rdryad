4de3868fee959e03df7ea5408448061a30a1577e
Karthik Ram <karthik.ram@gmail.com>
2012-02-28 17:23:16 -0800

    Added new function to update existing metadata file. Currently not working.

--- R/updatealldryad_metadata.R
+++ R/updatealldryad_metadata.R
@@ -1,27 +1,31 @@
 
-#`Update metadata for new Dryad oai's for defined time period to an existing csv file.
+#'Update metadata for new Dryad oai's for defined time period to an existing csv file.
 #' @import RCurl XML stringr ReadImages gdata plyr
-#`@param file file containing previous import
-#`@param  overwrite Will automatically overwrite existing file. To prevent overwrite, set to FALSE
-#`@param  newfile Name for new file to write updated data. If no name is given, new file will have same name as old file with timestamp appended.
-#`@export
-#`@examples \dontrun{
-#` updatealldrayad_metadata(file='~/path/to/data/dryad_metadata.csv',overwrite=T)
-#` updatealldrayad_metadata(file='~/path/to/data/dryad_metadata.csv',overwrite=F,overwrite=F,newfile='dryad_new')
-#` }
+#'@param file file containing previous import
+#'@param  overwrite Will automatically overwrite existing file. To prevent overwrite, set to FALSE
+#'@param  newfile Name for new file to write updated data. If no name is given, new file will have same name as old file with timestamp appended.
+#'@export
+#'@examples \dontrun{
+#' updatealldrayad_metadata(file='~/path/to/data/dryad_metadata.csv',overwrite=T)
+#' updatealldrayad_metadata(file='~/path/to/data/dryad_metadata.csv',overwrite=F,overwrite=F,newfile='dryad_new')
+#' }
 updatealldryad_metadata <- function(file, overwrite = T,
-    newfile = NULL) {
+    newfile = NULL,progress="text") {
     df <- read.csv(file, header = T)
-    cat(names(df))
     myoailist <- listidentifiers("r")  # get all oai's
     myoailist <- llply(myoailist[[1]], function(x) x$identifier)  # list of file
-    new_idenfiers <- setdiff(df$identifier, myoailist)
-    if (length(new_idenfiers) == 0) {
+    old.identifiers <- as.list(str_replace(df$identifier.1,"http://hdl.handle.net/","oai:datadryad.org:"))
+    current.identifiers <- myoailist
+    new_identifiers <- setdiff(old.identifiers,myoailist)
+    new_items <- length(new_identifiers)
+    if (length(new_identifiers) == 0) {
         stop("No new records available on Dryad\n")
     }
-    if (length(new_idenfiers) > 0) {
+    if (length(new_identifiers) > 0) {
+
+        # ERRORS ARE HERE
         new_metadata <- llply(new_identifiers, download_dryadmetadata,
-            tansform = T, .progress = progress)
+            transform = T, .progress = progress)
         new_metadata_ <- new_metadata_[unlist(lapply(new_metadata_,
             function(x) length(x$metadata)) != 0)]
         dryadtodf <- function(x) {
@@ -43,5 +47,7 @@ updatealldryad_metadata <- function(file, overwrite = T,
         new_filename = ifelse(is.null(newfile), paste(str_sub(file,
             1, -5), "_", timestamp, ".csv", sep = ""), newfile)
         save(df, file = new_filename)
+        status <-ifelse(overwrite==T,"existing file: ","new file:")
+        cat("Added",new_identifiers,"records to",status,new_filename,"\n")
     }
 }
