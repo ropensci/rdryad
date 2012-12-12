4b28742892083a8331dbb69cf3bd5013e6092b7d
Karthik Ram <karthik.ram@gmail.com>
2012-12-12 11:44:00 -0800

    Pushed a temporary fix for issue #4

--- R/download_url.r
+++ R/download_url.r
@@ -8,16 +8,21 @@
 #' @examples \dontrun{
 #' download_url('10255/dryad.1759')
 #' }
-download_url <- 
+download_url <-
 function(id, curl = getCurlHandle()) {
-    mets_metadata <- sprintf("%s/%s/%s", "http://datadryad.org/metadata/handle/", 
+    mets_metadata <- sprintf("%s/%s/%s", "http://datadryad.org/metadata/handle/",
         id, "/mets.xml")
     tt <- getURLContent(mets_metadata, curl = curl)
     page <- xmlParse(tt)
+    # The issue is here
     out <- xpathApply(page, "//mets:FLocat", function(x) {
         link <- xmlAttrs(x, "xlink:href")
         bitstream <- link["xlink:href"]
         sprintf("%s/%s", "http://datadryad.org", bitstream)
     })
+    if(length(out)>0) {
     out[lapply(out, str_detect, pattern = "sequence=1") == "TRUE"][[1]]
-} 
+        } else {
+            message("No output from search")
+        }
+}
