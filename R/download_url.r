75f390bc3c8ff10a499865699084ce3a2b38d58a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-16 14:04:46 -0800

    updated man files for new roxygen2 version

--- R/download_url.r
+++ R/download_url.r
@@ -1,27 +1,29 @@
-#' Download URL for a Dryad id.
-#' @import RCurl XML stringr
-#' @param id Dryad identifier, i.e. '10255/dryad.19'.
-#' @param curl If using in a loop, call getCurlHandle() first and pass
-#'    the returned value in here (avoids unnecessary footprint)
-#' @return A URL for dataset for the Dryad id.
+#' Get a URL given a Dryad ID
+#'
 #' @export
+#' @param id Dryad identifier, i.e. '10255/dryad.19'.
+#' @param ...	Curl options, passed on to \code{\link[httr]{GET}}
+#' @return A URL for dataset for the Dryad id.
 #' @examples \dontrun{
-#' download_url(id='10255/dryad.1759')
+#' download_url(id = '10255/dryad.1759')
 #' }
-download_url <- function(id, curl = getCurlHandle()) 
-{
-	mets_metadata <- sprintf("%s/%s/%s", "http://datadryad.org/metadata/handle/",
-													 id, "/mets.xml")
-	tt <- getURLContent(mets_metadata, curl = curl)
-	page <- xmlParse(tt)
+download_url <- function(id, ...) {
+	mm <- sprintf("%s/%s/%s", "http://datadryad.org/metadata/handle/", id, "/mets.xml")
+	tt <- content(GET(mm), "text")
+	page <- xml2::read_xml(tt)
+	out <- xml2::xml_find_all(page, "//mets:FLocat", xml_ns(page)[1])
+	xml2::xml_attr(out, "xlink:href")
+
+	# tt <- getURLContent(mm, curl = curl)
+	page <- XML::xmlParse(tt)
 	# The issue is here
 	out <- xpathApply(page, "//mets:FLocat", function(x) {
 		link <- xmlAttrs(x, "xlink:href")
 		bitstream <- link["xlink:href"]
 		sprintf("%s/%s", "http://datadryad.org", bitstream)
 	})
-	if(length(out)>0) {
-		out[lapply(out, str_detect, pattern = "sequence=1") == "TRUE"][[1]]
+	if (length(out) > 0) {
+		out[grepl("sequence=1", out)][[1]]
 	} else {
 		message("No output from search")
 	}
