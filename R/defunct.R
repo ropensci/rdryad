9cc42b35b621de5e74abc2d05429c25e87a9478a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-16 14:26:15 -0800

    defunct functions file and man pages

---
+++ R/defunct.R
@@ -0,0 +1,17 @@
+#' Download metadata for all Dryad oai's for defined time period
+#'
+#' This function is defunct
+#'
+#' @keywords internal
+getalldryad_metadata <- function(...) {
+  .Defunct(msg = "This function is defunct. Use OAI-PMH via dr_*() functions or Solr based search via d_*() functions")
+}
+
+#' Search metadata for search terms using regex
+#'
+#' This function is defunct
+#'
+#' @keywords internal
+search_dryad <- function(...) {
+  .Defunct(msg = "This function is defunct. Use OAI-PMH via dr_*() functions or Solr based search via d_*() functions")
+}
