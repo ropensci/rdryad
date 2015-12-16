55887af4256b6a6c193a3ea71ff640bcf96d409f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-16 14:04:37 -0800

    added pkg level man file

---
+++ R/rdryad-package.R
@@ -0,0 +1,12 @@
+#' Interface to the Solr API for Dryad, and the API to get datasets.
+#'
+#' @importFrom httr GET content stop_for_status http_status write_disk
+#' @importFrom XML getNodeSet xmlAttrs xmlSApply xmlValue xmlParse htmlParse
+#' xpathApply xpathSApply xmlToList
+#' @importFrom jsonlite toJSON fromJSON
+#' @importFrom oai id list_identifiers list_records list_metadataformats
+#' list_sets get_records
+#' @name rdryad-package
+#' @aliases rdryad
+#' @docType package
+NULL
