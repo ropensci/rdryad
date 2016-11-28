639aa366eb2ae717e93c8520d1638473d770fe89
Scott Chamberlain <myrmecocystus@gmail.com>
2016-11-28 13:46:29 -0800

    fixed tsts for new oai output, use new oai in description file

--- tests/testthat/test-dr_get_records.R
+++ tests/testthat/test-dr_get_records.R
@@ -10,15 +10,22 @@ test_that("dr_get_records works", {
   aa <- suppressWarnings(dr_get_records(id))
   bb <- suppressWarnings(dr_get_records(ids))
 
-  expect_is(aa, "data.frame")
-  expect_is(aa$identifier, "character")
+  expect_is(aa, "list")
+  expect_is(aa[[1]], "list")
+  expect_is(aa[[1]]$header, "tbl_df")
+  expect_is(aa[[1]]$metadata, "tbl_df")
+  expect_match(aa[[1]]$header$identifier, "oai:datadryad.org:10255/dryad.8820")
+  expect_match(aa[[1]]$header$datestamp, "[0-9]{4}-[0-9]{2}-[0-9]{2}")
   expect_equal(NROW(aa), length(id))
-  expect_true(grepl("dryad", aa$identifier))
 
-  expect_is(bb, "data.frame")
-  expect_is(bb$identifier, "character")
+  expect_is(bb, "list")
+  expect_is(bb[[1]], "list")
+  expect_is(bb[[1]]$header, "tbl_df")
+  expect_is(bb[[1]]$metadata, "tbl_df")
+  expect_match(bb[[1]]$header$identifier, "oai:datadryad.org:10255/dryad.36217")
+  expect_match(bb[[1]]$header$datestamp, "[0-9]{4}-[0-9]{2}-[0-9]{2}")
   expect_equal(NROW(bb), length(ids))
-  expect_true(any(grepl("dryad", bb$identifier)))
+  expect_true(all(grepl("dryad", vapply(bb, "[[", "", c("header", "identifier")))))
 })
 
 test_that("dr_get_records fails as expected", {
