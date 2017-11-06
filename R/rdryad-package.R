8de28998cadd3c3ce8d4c0d07c0b28fc26264a8d
Scott Chamberlain <myrmecocystus@gmail.com>
2017-11-06 09:01:52 -0800

    udpate man files to new roxygen2, bump ver update year in license file remove covr from suggests, install on travis use crul instead of httr remove utils from imports

--- R/rdryad-package.R
+++ R/rdryad-package.R
@@ -4,7 +4,6 @@
 #' @importFrom xml2 read_xml xml_find_all xml_ns xml_attr
 #' @importFrom oai id list_identifiers list_records list_metadataformats
 #' list_sets get_records
-#' @importFrom utils download.file
 #' @name rdryad-package
 #' @aliases rdryad
 #' @docType package
