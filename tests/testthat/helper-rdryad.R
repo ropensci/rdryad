6556a9223fe0173c82ccec6df1223f639d7fac44
Scott Chamberlain <sckott@fastmail.com>
2020-12-15 11:25:54 -0800

    re-record some tests to fix them

--- tests/testthat/helper-rdryad.R
+++ tests/testthat/helper-rdryad.R
@@ -5,7 +5,7 @@ vcr::check_cassette_names()
 ogpath <- rdryad_cache$cache_path_get()
 
 testthat::setup({
-  rdryad_cache$cache_path_set(full_path="tests/files")
+  rdryad_cache$cache_path_set(full_path="../files")
 })
 
 testthat::teardown({
