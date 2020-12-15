33bf3a820d9e4673eafdce2c5d8ab0fd47f44f9d
Scott Chamberlain <sckott@fastmail.com>
2020-12-15 12:21:58 -0800

    update man file

--- R/files.R
+++ R/files.R
@@ -23,9 +23,8 @@ dryad_files <- function(ids, ...) {
 #' @param ids (numeric) one or more file ids, required
 #' @param ... Further args passed on to [crul::verb-GET]
 #' @return a list of lists, each named by the input DOI
-#' @note there's no use of cached files here because we can't 
-#' determine what the file path will be before making the request,
-#' which requires knowing what the data type the file will be
+#' @note UPDATE: we used to not use caching in this fxn; we do now
+#' as of 2020-12-15
 #' @examples \dontrun{
 #' dryad_files_download(ids = 61858)
 #' dryad_files_download(ids = 61859)
